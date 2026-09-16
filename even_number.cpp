486521d9f415a9b4eb03fcad1453906f2d9c11f1
Salohiddin <symplecticfoundryvibe@gmail.com>
2026-09-16 17:18:04 +0900

    Add program to print even numbers up to 100
    Implement a program to print even numbers from 1 to 100.

---
+++ even_number.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i=1;
+    do
+    {
+        if(i%2==0)
+        cout<<i<<"\t";
+        i++;   
+    } while(i<=100);
+    return 0;
+}
